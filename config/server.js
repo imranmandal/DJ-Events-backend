a39316ef4035e7dc57fe5003b18b7f6ccfd04b4d
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-06-07 15:39:32 +0530

    CRUD operation for events done

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5255923c3dfd5fd6c644450745a26762'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2f242811-759f-430a-843c-8c56252cc46b'
+};
